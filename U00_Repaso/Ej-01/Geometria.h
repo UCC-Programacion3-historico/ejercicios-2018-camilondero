c4d5d757fa6ae6e18e04694c936d2809fcb447ac
camilondero <cami_londero@hotmail.com>
2019-08-08 21:04:30 -0300

    resolucion

--- U00_Repaso/Ej-01/Geometria.h
+++ U00_Repaso/Ej-01/Geometria.h
@@ -1,8 +1,47 @@
 #ifndef REPASO_GEOMETRIA_H
 #define REPASO_GEOMETRIA_H
 
-
+template<class T>
 class Geometria {
+private:
+    T alto;
+    T ancho;
+    T color;
+
+public:
+
+    Geometria(T al ,T an ,T c){
+        alto=al;
+        ancho=an;
+        color=c;
+
+    };
+
+    virtual void superficie() = 0;
+
+    virtual void perimetro() = 0;
+
+    T getalto() {
+        return alto;
+    };
+
+    T getancho() {
+        return ancho;
+    };
+
+    T getcolor() {
+        return color;
+    };
+
+    T getsuperficie() {
+        return superficie();
+    };
+
+    T getperimetro() {
+        return perimetro();
+
+    };
+
 
 };
 
--- U00_Repaso/Ej-01/Triangulo.h
+++ U00_Repaso/Ej-01/Triangulo.h
@@ -1,8 +1,30 @@
 #ifndef REPASO_TRIANGULO_H
 #define REPASO_TRIANGULO_H
 
+#include "Geometria.h"
 
-class Triangulo {
+template <class T>
+
+class Triangulo:public Geometria {
+
+private:
+    float angulo;
+public:
+    Triangulo(T al, T an, T c, float alfa):Geometria(al,an,c){angulo=alfa;}
+
+    float getangulo(){
+        return angulo;
+    };
+
+    virtual void superficie(){
+        sup=0;
+        sup=(getalto()*getancho()/2);
+    }
+
+    virtual void perimetro(){
+        per=0;
+        per=
+    }
 
 };
 
