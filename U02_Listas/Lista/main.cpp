16b868434d4bb7dee5cc0f3b3f6fcfbe06be64b8
camilondero <cami_londero@hotmail.com>
2019-08-21 20:00:59 -0300

    agrega listas

---
+++ U02_Listas/Lista/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Lista.h"
+using namespace std;
+
+int main(){
+    Lista<int> l;
+
+    l.insertarPrimero(3);
+    l.insertarPrimero(2);
+    l.insertarPrimero(1);
+
+    cout << "Hola mindo"<< l.getTamanio() << endl;
+
+
+    return 0;
+}
